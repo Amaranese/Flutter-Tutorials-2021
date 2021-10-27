ddb5a81a0c9ac2248c4de6b9463aec2399134d3d
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-27 13:45:40 +0200

    Create global.dart

---
+++ 21-Make a CRUD TodoList app with networking Restful API in Flutter/code/myapp/lib/global.dart
@@ -0,0 +1,13 @@
+/*
+Nguyen Duc Hoang (Mr)
+Programming tutorial channel:
+https://www.youtube.com/c/nguyenduchoang
+Flutter, React, React Native, IOS development, Swift, Python, Angular
+* */
+//const SERVER_NAME = "10.0.2.2";
+//const SERVER_NAME = "localhost";
+//const SERVER_NAME = "127.0.0.1";
+const SERVER_NAME = "192.168.1.34";//your server's ip address
+const URL_TODOS = "http://$SERVER_NAME:3000/todos";
+const URL_TASKS = 'http://$SERVER_NAME:3000/tasks';
+const URL_TASKS_BY_TODOID = 'http://$SERVER_NAME:3000/tasks/todoid/';
